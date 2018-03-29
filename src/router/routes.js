0833c1be213c0759a5600ccc4a7427e14d6c239d
Milos Miljanic <milosmilos96@gmail.com>
2018-03-29 18:21:45 +0200

    initial setup

---
+++ src/router/routes.js
@@ -0,0 +1,44 @@
+
+import Home from 'Components/Home.component';
+import Login from 'Components/Login.component';
+import Register from 'Components/Register.component';
+import Main from 'Components/Main.component';
+
+export const routes = [
+  {
+    path: '/',
+    name: 'main',
+    component: Main,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+    meta: {
+      logged: true,
+    },
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register,
+    meta: {
+      logged: true,
+    },
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: Home,
+    meta: {
+      logged: true,
+    },
+  },
+  {
+    path: '/logout',
+    name: 'logout',
+    meta: {
+      logged: true,
+    },
+  },
+];
