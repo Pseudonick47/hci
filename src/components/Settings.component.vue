a9fe11a0db33618b7c9076d1e7103739b29091b7
Simona <167simonap@gmail.com>
2018-04-05 00:51:30 +0200

    currency changing

--- src/components/Settings.component.vue
+++ src/components/Settings.component.vue
@@ -8,7 +8,8 @@
     >
       <v-btn
         color="grey"
-        dark slot="activator"
+        dark
+        slot="activator"
       >Settings</v-btn>
       <v-card>
         <v-list>
@@ -23,6 +24,15 @@
                 ></v-text-field>
               </v-layout>
             </v-list-tile-action>
+          </v-list-tile>
+          <v-divider></v-divider>
+          <v-list-tile>
+            <v-subheader>Change currency</v-subheader>
+            <v-btn
+              color="blue"
+              dark
+              @click="showCurrencyDialog"
+            >{{ currencyValue }}</v-btn>
           </v-list-tile>
           <v-divider></v-divider>
           <v-list-tile>
@@ -50,11 +60,6 @@
             flat
             @click="menu = false"
           >Cancel</v-btn>
-          <v-btn
-            color="yellow"
-            flat
-            @click="menu = false"
-            >Save</v-btn>
         </v-card-actions>
       </v-card>
     </v-menu>
@@ -62,27 +67,40 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      menu: false
-    }),
-    computed: {
-      draggable: {
-        get() {
-          return this.$store.getters.draggable;
-        },
-        set() {
-          this.$store.commit('changeDraggable');
-        }
+export default {
+  data: () => ({
+    menu: false
+  }),
+  methods: {
+    showCurrencyDialog() {
+      this.$store.commit('changeVisibilityCurrencyDialog', true);
+    }
+  },
+  computed: {
+    draggable: {
+      get() {
+        return this.$store.getters.draggable;
       },
-      resizable: {
-        get() {
-          return this.$store.getters.resizable;
-        },
-        set() {
-          this.$store.commit('changeResizable');
-        }
+      set() {
+        this.$store.commit('changeDraggable');
+      }
+    },
+    resizable: {
+      get() {
+        return this.$store.getters.resizable;
+      },
+      set() {
+        this.$store.commit('changeResizable');
+      }
+    },
+    currencyValue: {
+      get() {
+        return this.$store.getters.currencyValue;
+      },
+      set(value) {
+        this.$store.commit('changeCurrency', value);
       }
     }
-  };
+  }
+};
 </script>
--- src/store/modules/settings.store.js
+++ src/store/modules/settings.store.js
@@ -2,11 +2,15 @@
 const state = {
     draggable: true,
     resizable: true,
+    currencyValue: 'EUR',
+    currencyDialog: false
 };
 
 const getters = {
     draggable: (state) => state.draggable,
-    resizable: (state) => state.resizable
+    resizable: (state) => state.resizable,
+    currencyValue: (state) => state.currencyValue,
+    currencyDialog: (state) => state.currencyDialog
 };
 
 const mutations = {
@@ -16,6 +20,14 @@ const mutations = {
     changeResizable(state) {
         state.resizable = !state.resizable;
     },
+    changeCurrency(state, value) {
+        state.currencyValue = value;
+    },
+    changeVisibilityCurrencyDialog(state, value) {
+        if (value === true || value === false) {
+            state.currencyDialog = value;
+        }
+    }
 };
 
 export {
